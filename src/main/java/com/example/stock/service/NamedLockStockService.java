ad30d859d15803aff1dc568e7095cbe9504e0a69
Jeongyeon Kim <wjddus9132@gmail.com>
2025-04-20 14:28:03 +0900

    feat: Named Lock 적용

--- src/main/java/com/example/stock/service/NamedLockStockService.java
+++ src/main/java/com/example/stock/service/NamedLockStockService.java
@@ -7,24 +7,15 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class StockService {
+public class NamedLockStockService {
 
     private final StockRepository stockRepository;
 
-    public StockService(StockRepository stockRepository) {
+    public NamedLockStockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
-    /**
-     * @Transactional을 사용하면 StockService를 wrapping 한 클래스를 새로 만들어서 사용
-     * Tx 시작 -> 로직 실행 -> Tx 종료(DB 업데이트)
-     * ----------------------------------------------------------------------
-     * Tx의 종료 시점에 DB에 업데이트 하는데
-     * stockService.decrease가 완료되고 실제 DB에 업데이트 되기 전에
-     * 다른 스레드가 stockService.decrease 호출 가능
-     * -> 다른 스레드는 갱신 전 값을 가져가기 때문에 동시성 이슈 발생
-     */
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void decrease(Long id, Long quantity) {
         // Stock 조회
         Stock stock = stockRepository.findById(id)
